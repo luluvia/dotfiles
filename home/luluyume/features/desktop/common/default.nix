f7e1f7f370d1189a5cc153014dbb26251906e060
Lulu Yume <lu@luyu.me>
2023-09-16 23:33:59 -0500

    Add GitKraken

--- home/luluyume/features/desktop/common/default.nix
+++ home/luluyume/features/desktop/common/default.nix
@@ -3,6 +3,7 @@
     ./1password.nix
     ./firefox-devedition.nix
     ./font.nix
+    ./gitkraken.nix
     ./jbtoolbox.nix
   ];
 }
---
+++ home/luluyume/features/desktop/common/gitkraken.nix
@@ -0,0 +1,3 @@
+{ pkgs, ... }: {
+  home.pkgs = with pkgs; [ gitkraken ];
+}
